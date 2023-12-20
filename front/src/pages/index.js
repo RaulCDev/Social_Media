6cc312e5140fbaeb42bf5f40eadea9378a910854
Raudios <Raul_Conde_Rodriguez@hotmail.com>
2023-12-20 22:34:55 +0100

    Añadiendo la carpeta front

---
+++ front/src/pages/index.js
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <>
+      <div className='icons'>
+        <a href="https://github.com/RaulCDev">
+          <img src="github.svg" alt="logo_github"/>
+        </a>
+        <a href="https://www.linkedin.com/in/ra%C3%BAl-conde-rodr%C3%ADguez/">
+          <img src="linkedin.svg" alt="logo_linkedin"/>
+        </a>
+      </div>
+      <p className='title'>Twittter</p>
+      <div className='main_text'>
+        <p>Content :)</p>
+      </div>
+    </>
+  )
+}
