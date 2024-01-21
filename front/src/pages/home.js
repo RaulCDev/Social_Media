389ca1335243cf099c28101c5d58f2e504294169
Raudios <Raul_Conde_Rodriguez@hotmail.com>
2024-01-21 22:19:00 +0100

    Button to watch the token in the console

--- front/src/pages/home.js
+++ front/src/pages/home.js
@@ -8,53 +8,30 @@ export default function Home() {
   const [error, setErrorMessage] = useState('');
 
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    // Construir el objeto JSON con los datos del formulario
-    const data = {
-      username: username,
-      password: password,
-    };
-
-    try {
-      // Realizar la solicitud a la API para enviar los datos en formato JSON
-      const response = await fetch('http://localhost:5000/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-      });
-
-      // Manejar la respuesta de la API según tus necesidades
-      const responseData = await response.json();
-      if (responseData.success == true) {
-        setMessage(responseData.message);
-        setErrorMessage('');
-      } else {
-        setErrorMessage(responseData.message);
-        setMessage('');
-      }
-    } catch (error) {
-      console.log('Error al realizar la solicitud a la API:', error);
-      setErrorMessage('Error al realizar la solicitud a la API');
-    };
+  const handleTokenButtonClick = () => {
+    // Buscar si existe el archivo
+    if (localStorage.getItem('token')) {
+      const token = localStorage.getItem('token');
+      console.log('Token encontrado en localStorage:', token);
+    } else {
+      console.log('No se encontró el token en localStorage');
+    }
   }
-    return (
-      <>
-        <div className='icons'>
-          <a href="https://github.com/RaulCDev">
-            <img src="github.svg" alt="logo_github"/>
-          </a>
-          <a href="https://www.linkedin.com/in/ra%C3%BAl-conde-rodr%C3%ADguez/">
-            <img src="linkedin.svg" alt="logo_linkedin"/>
-          </a>
-        </div>
-        <p className='title'>x?</p>
-        <div className='main_text'>
-            <p>Hola buenas</p>
-        </div>
-      </>
-    );
+  return (
+    <>
+      <div className='icons'>
+        <a href="https://github.com/RaulCDev">
+          <img src="github.svg" alt="logo_github"/>
+        </a>
+        <a href="https://www.linkedin.com/in/ra%C3%BAl-conde-rodr%C3%ADguez/">
+          <img src="linkedin.svg" alt="logo_linkedin"/>
+        </a>
+      </div>
+      <p className='title'>x?</p>
+      <div className='main_text'>
+        <p>Hola buenas</p>
+        <button onClick={handleTokenButtonClick}>Token</button>
+      </div>
+    </>
+  );
 }
