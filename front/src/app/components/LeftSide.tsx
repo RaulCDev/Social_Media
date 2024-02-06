4b264d460e8b98bd9a96af61df57bff027c1d4fa
Raudios <Raul_Conde_Rodriguez@hotmail.com>
2024-02-06 19:48:24 +0100

    The More Options button doesnt work

--- front/src/app/components/LeftSide.tsx
+++ front/src/app/components/LeftSide.tsx
@@ -1,7 +1,8 @@
 'use client';
+import { useState } from 'react';
 import { Card, CardHeader, CardBody, Avatar, CardFooter } from '@nextui-org/react'
 import Link from 'next/link'
-import { IconBrandLinkedin, IconBrandGithub, IconDots, IconBrandX, IconDotsCircleHorizontal ,IconUser ,IconHome, IconSearch, IconBell, IconMail, IconNotes, IconBookmark, IconUsers } from '@tabler/icons-react'
+import { IconSettings ,IconExternalLink ,IconVip ,IconCashBanknote ,IconBrandLinkedin, IconBrandGithub, IconDots, IconBrandX, IconDotsCircleHorizontal ,IconUser ,IconHome, IconSearch, IconBell, IconMail, IconNotes, IconBookmark, IconUsers } from '@tabler/icons-react'
 import {Button} from '@nextui-org/button';
 
 export default function LeftSide ({
@@ -16,41 +17,79 @@ export default function LeftSide ({
     content: string
   }) {
 
+    const [popoverOpen, setPopoverOpen] = useState(false);
+    const [buttonPosition, setButtonPosition] = useState({ top: 0, left: 0 });
+
+    const handleTogglePopover = (event: React.MouseEvent<HTMLButtonElement>) => {
+      const rect = event.currentTarget.getBoundingClientRect();
+      setButtonPosition({ top: rect.bottom, left: rect.left });
+      setPopoverOpen(!popoverOpen);
+    };
+
     const buttons = [
       { icon: <IconHome className='w-8 h-8' />, text: 'Home' },
       { icon: <IconSearch className='w-8 h-8' />, text: 'Search' },
       { icon: <IconBell className='w-8 h-8' />, text: 'Notificaciones' },
       { icon: <IconMail className='w-8 h-8' />, text: 'Mensajes' },
       { icon: <IconNotes className='w-8 h-8' />, text: 'Lists' },
-      { icon: <IconBookmark className='w-8 h-8' />, text: 'Guardado' },
-      { icon: <IconUsers className='w-8 h-8' />, text: 'Comunidades' },
       { icon: <IconBrandX className='w-8 h-8' />, text: 'Premium' },
       { icon: <IconUser className='w-8 h-8' />, text: 'Perfil' },
-      { icon: <IconDotsCircleHorizontal className='w-8 h-8' />, text: 'Mas opciones' },
     ];
 
+    const altButtons = [
+      { icon: <IconBookmark className='w-8 h-8' />, text: 'Guardado' },
+      { icon: <IconUsers className='w-8 h-8' />, text: 'Comunidades' },
+      { icon: <IconCashBanknote className='w-8 h-8' />, text: 'Monetizacion' },
+      { icon: <IconVip className='w-8 h-8' />, text: 'Premium' },
+      { icon: <IconExternalLink  className='w-8 h-8' />, text: 'Anuncios' },
+      { icon: <IconSettings className='w-8 h-8' />, text: 'Configuracion y privacidad' },
+    ]
+
     return (
-        <div className="fixed top-0 left-0 ml-10 mt-10 z-50">
+        <div className="fixed top-0 left-0 ml-3 mt-3 z-10">
           <div className="icons">
-            <IconBrandX className='w-8 h-8' />
-            <a href="https://www.linkedin.com/in/ra%C3%BAl-conde-rodr%C3%ADguez/" target="_blank" rel="noopener noreferrer">
-              <IconBrandGithub className='w-8 h-8' />
-            </a>
-            <a href="https://github.com/RaulCDev" target="_blank" rel="noopener noreferrer">
-              <IconBrandLinkedin className='w-8 h-8' />
-            </a>
+            <button className="iconButton">
+              <IconBrandX className='myLinks' />
+            </button>
+            <button className="iconButton">
+              <a href="https://www.linkedin.com/in/ra%C3%BAl-conde-rodr%C3%ADguez/" target="_blank" rel="noopener noreferrer">
+                <IconBrandLinkedin className='myLinks' />
+              </a>
+            </button>
+            <button className="iconButton">
+              <a href="https://github.com/RaulCDev" target="_blank" rel="noopener noreferrer">
+                <IconBrandGithub className='myLinks' />
+              </a>
+            </button>
           </div>
           {/* Botones */}
           {buttons.map((button, index) => (
-            <Button key={index} className='try'>
+            <Button key={index} className='try' radius="full" variant="light">
               {button.icon}
               <span>{button.text}</span>
             </Button>
           ))}
-          <Button className='bg-green-500'>
+          {/* Botón de "Más opciones" */}
+          <Button onClick={handleTogglePopover} className='try' radius="full" variant="light">
+            <IconDotsCircleHorizontal className='w-8 h-8' />
+            <span>Mas opciones</span>
+          </Button>
+          {/* Ventana emergente */}
+          {popoverOpen && (
+            <div className="popover" style={{ top: buttonPosition.top, left: buttonPosition.left }}>
+              {/* Opciones */}
+              {altButtons.map((button, index) => (
+                <Button key={index} className='try' radius="full" variant="light">
+                  {button.icon}
+                   <span>{button.text}</span>
+                </Button>
+              ))}
+            </div>
+          )}
+          <Button className='bg-green-500' radius="full">
             Post
           </Button>
-          <Button className="flex gap-x-2">
+          <Button className="flex gap-x-2" radius="full" variant="light">
             <Avatar radius="full" size="md" src={avatarUrl} />
             <div className="flex flex-col gap-1 items-start justify-center">
               <h4 className="text-small font-semibold leading-none text-default-600">{userFullName}</h4>
--- front/src/app/home/page.tsx
+++ front/src/app/home/page.tsx
@@ -10,6 +10,7 @@ export default function Home() {
   return (
     <React.Fragment>
       <div className="content-center flex min-h-screen flex-col items-center justify-between style={{ display: inline }}">
+        <LeftSide  userFullName="Manolo" userName="manolo" avatarUrl="https://github.com/RaulCDev.png" content="Hola"/>
         <div className='max-w-[600px] w-full mx-auto border-l border-r border-white/20 min-h-screen'>
           <PostTipes />
           <WritePost userFullName="Manolo" userName="manolo" avatarUrl="https://github.com/RaulCDev.png" content="Post"/>
@@ -19,8 +20,6 @@ export default function Home() {
           <Account_Card userFullName="Manolo" userName="manolo" avatarUrl="https://github.com/RaulCDev.png" content="Mucho Texto Mucho TextoMucho TextoMucho TextoMucho TextoMucho TextoMucho TextoMucho TextoMucho TextoMucho TextoMucho TextoMucho TextoMucho TextoMucho TextoMucho TextoMucho TextoMucho TextoMucho TextoMucho Texto" />
           <Account_Card userFullName="Manolo" userName="manolo" avatarUrl="https://github.com/RaulCDev.png" content="Mucho Texto Mucho TextoMucho TextoMucho TextoMucho TextoMucho TextoMucho TextoMucho TextoMucho TextoMucho TextoMucho TextoMucho TextoMucho TextoMucho TextoMucho TextoMucho TextoMucho TextoMucho TextoMucho Texto" />
         </div>
-        <Icons />
-        <LeftSide  userFullName="Manolo" userName="manolo" avatarUrl="https://github.com/RaulCDev.png" content="Hola"/>
       </div>
     </React.Fragment>
   );
