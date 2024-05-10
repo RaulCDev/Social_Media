b6fed97c4d13e46706ff23231a34297b919eb177
RaulCDev <Raul_Conde_Rodriguez@hotmail.com>
2024-05-10 17:28:07 +0200

    Resize fixed

--- front/src/app/github_login/page.tsx
+++ front/src/app/github_login/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { usePathname, useSearchParams, useRouter} from 'next/navigation';
 import { useEffect, useState } from 'react';
+import { PacmanLoader } from "react-spinners";
 
 const GithubLogin = () => {
   const pathname = usePathname();
@@ -12,7 +13,6 @@ const GithubLogin = () => {
     const code = searchParams.get("code");
 
     if (code) {
-      // Realizar la solicitud al backend con el código
       fetch('http://127.0.0.1:5000/github_callback', {
         method: 'POST',
         headers: {
@@ -23,8 +23,6 @@ const GithubLogin = () => {
       .then(response => response.json())
       .then(data => {
         setToken(data.access_token);
-        // Almacenar el token JWT en el localStorage
-        console.log(data.access_token);
         localStorage.setItem('token', data.access_token);
         router.push('/home');
       })
@@ -36,7 +34,7 @@ const GithubLogin = () => {
 
   return (
     <div>
-      <p>{token}</p>
+      <PacmanLoader color="#36d7b7" />
     </div>
   );
 };
