dff221cb973900c11a83aa1d0966a3eccde21ea2
RaulCDev <Raul_Conde_Rodriguez@hotmail.com>
2025-09-05 19:40:25 +0200

    stage

--- front/src/app/components/PostCards/PostCards.tsx
+++ front/src/app/components/PostCards/PostCards.tsx
@@ -1,62 +1,62 @@
-import React, { useState, useEffect } from 'react'
-import Post_Card from './PostCard'
+import React, { useState, useEffect } from "react";
+import Post_Card from "./PostCard";
 
 type Post_CardProps = {
-  id: number
-  userFullName: string
-  userName: string
-  avatarUrl: string
-  content: string
-}
-
-type Post_CardComponent = React.ComponentType<Post_CardProps>
+  id: number;
+  userFullName: string;
+  userName: string;
+  avatarUrl: string;
+  content: string;
+};
 
 export default function Post_Cards() {
-  const limit = 10
-  const [cards, setCards] = useState<JSX.Element[]>(new Array(limit).fill(null))
-  const token = localStorage.getItem('token');
+  const limit = 10;
+  const [cards, setCards] = useState<JSX.Element[]>(
+    new Array(limit).fill(null)
+  );
+  const token = localStorage.getItem("token");
 
   const fetchCards = async () => {
     const response = await fetch(`http://localhost:5000/cards`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-          'Authorization': `Bearer ${token}`,
-          'Content-Type': 'application/json'
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
       },
-  });
-    const newCards = await response.json()
-    return newCards
-  }
+    });
+    const newCards = await response.json();
+    return newCards;
+  };
 
   const loadMoreCards = async () => {
-    const newCards = await fetchCards()
+    const newCards = await fetchCards();
     const newCardsComponents = newCards.map((cardData: any) => (
-        <Post_Card
-          key={cardData.id}
-          id={cardData.id}
-          userFullName={cardData.userFullName}
-          userName={cardData.userName}
-          avatarUrl={cardData.avatarUrl}
-          content={cardData.content}
-          likes_amount={cardData.likes}
-          views_amount={cardData.views}
-          comments_amount={cardData.comments}
-          isLiked={cardData.isLiked}
-        />
+      <Post_Card
+        key={cardData.id}
+        id={cardData.id}
+        userFullName={cardData.userFullName}
+        userName={cardData.userName}
+        avatarUrl={cardData.avatarUrl}
+        content={cardData.content}
+        likes_amount={cardData.likes}
+        views_amount={cardData.views}
+        comments_amount={cardData.comments}
+        isLiked={cardData.isLiked}
+      />
     ));
-    setCards((prevCards) => [...prevCards, ...newCardsComponents])
-  }
+    setCards((prevCards) => [...prevCards, ...newCardsComponents]);
+  };
 
   useEffect(() => {
-    loadMoreCards()
-  }, [])
+    loadMoreCards();
+  }, []);
 
   return (
     <div>
-        {cards.filter(Boolean).map((card) => (
-            <React.Fragment key={card.key}>{card}</React.Fragment>
-        ))}
-        <button onClick={loadMoreCards}>Cargar más tarjetas</button>
+      {cards.filter(Boolean).map((card) => (
+        <React.Fragment key={card.key}>{card}</React.Fragment>
+      ))}
+      <button onClick={loadMoreCards}>Cargar más tarjetas</button>
     </div>
-  )
+  );
 }
